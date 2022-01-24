19a9da4a45707919cb88474ab8730f937b578ba5
erfangumelar <erfangumelar@gmail.com>
2022-01-24 17:21:48 +0700

    memasukan template video 12 finish

---
+++ resources/views/home/parsial/menu.blade.php
@@ -0,0 +1,8 @@
+<nav id="navbar" class="navbar">
+    <ul>
+        <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+        <li><a class="nav-link scrollto" href="#about">Login</a></li>
+        <li><a class="getstarted scrollto" href="#about">Daftar</a></li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+</nav><!-- .navbar -->
